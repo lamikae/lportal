52062b0f19b6a41239b7e0861ae885681e90c946
Mikael Lammentausta <mikael.lammentausta+github+@gmail.com>
2009-10-21 20:24:57 +0300

    load dependencies for testing

---
+++ test/test_helper.rb
@@ -0,0 +1,13 @@
+require 'test/rails_gem_chooser'
+RailsGemChooser.__load_gem('activerecord','2.3.4')
+RailsGemChooser.__load_gem('activesupport','2.3.4')
+
+class ActiveSupport::TestCase
+
+  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
+  #
+  # Note: You'll currently still have to declare fixtures explicitly in integration tests
+  # -- they do not yet inherit this setting
+#   fixtures :all
+
+end
